ee3d5d30beaf344f8bcc6dd93cf53a75f52251ee
kmoon <kristy@amazee.com>
2017-10-18 12:39:24 -0500

    TXC-31: Style prospectus, hover state and add pdf

--- frontend/app/screens/Home/index.js
+++ frontend/app/screens/Home/index.js
@@ -6,10 +6,12 @@ import Header from 'Header';
 import Logo from 'Logo';
 import NewsletterForm from 'NewsletterForm';
 import backgroundImage from '../../shared/images/texas-camper.png';
+import download from '../../shared/images/download.png';
+import prospectus from '../../shared/documents/prospectus.pdf';
 import styles from './styles.css';
 
-const Home = (): React.Element<any> =>
-  (<div className={styles.wrapper}>
+const Home = (): React.Element<any> => (
+  <div className={styles.wrapper}>
     <Helmet title="Texas Camp 2018" />
     <div className={styles.backgroundWrapper}>
       <img className={styles.backgroundImage} src={backgroundImage} alt="" />
@@ -18,8 +20,12 @@ const Home = (): React.Element<any> =>
     <Logo />
     <NewsletterForm />
     <div className={styles.prospectus}>
-      <a href="/">Sponsor Prospectus</a>
+      <a href={prospectus}>
+        <img src={download} alt="Download the Sponsor Prospectus" />
+        Sponsor Prospectus
+      </a>
     </div>
-  </div>);
+  </div>
+);
 
 export default Home;
--- frontend/app/shared/css/variables.js
+++ frontend/app/shared/css/variables.js
@@ -1,4 +1,5 @@
 module.exports = {
   orange: '#ef5a47',
   navy: '#10363f',
+  almostWhite: '#fbfaf7',
 };
