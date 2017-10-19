a2f4896b90ef5b0d7a698b60283f5eaf83558665
kmoon <kristy@amazee.com>
2017-10-19 11:50:57 -0500

    Adjust styles

--- frontend/app/screens/Home/index.js
+++ frontend/app/screens/Home/index.js
@@ -11,8 +11,8 @@ import backgroundImage from '../../shared/images/texas-camper.png';
 import download from '../../shared/images/download.png';
 import styles from './styles.css';
 
-const Home = (): React.Element<any> =>
-  (<div className={styles.wrapper}>
+const Home = (): React.Element<any> => (
+  <div className={styles.wrapper}>
     <Helmet title="Texas Camp 2018" />
     <div className={styles.backgroundWrapper}>
       <img className={styles.backgroundImage} src={backgroundImage} alt="" />
@@ -24,8 +24,10 @@ const Home = (): React.Element<any> =>
       <a href={prospectus}>
         <img src={download} alt="Download the Sponsor Prospectus" />
         Sponsor Prospectus
+        <div className={styles.border} />
       </a>
     </div>
-  </div>);
+  </div>
+);
 
 export default Home;
--- frontend/app/shared/css/variables.js
+++ frontend/app/shared/css/variables.js
@@ -1,5 +1,4 @@
 module.exports = {
   orange: '#ef5a47',
   navy: '#10363f',
-  almostWhite: '#fbfaf7',
 };
