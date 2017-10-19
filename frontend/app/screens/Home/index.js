80ab62c396b3b6613078146e8038577da7d4d19c
kmoon <kristy@amazee.com>
2017-10-19 16:46:30 -0500

    Add parallaxing on background images

--- frontend/app/screens/Home/index.js
+++ frontend/app/screens/Home/index.js
@@ -5,18 +5,16 @@ import Helmet from 'react-helmet';
 import Header from 'Header';
 import Logo from 'Logo';
 import NewsletterForm from 'NewsletterForm';
+import BackgroundImage from 'BackgroundImage';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import prospectus from '!file-loader?name=TexasCamp_2018_Sponsor_Prospectus_[hash].[ext]!documents/prospectus.pdf';
-import backgroundImage from '../../shared/images/texas-camper.png';
 import download from '../../shared/images/download.png';
 import styles from './styles.css';
 
 const Home = (): React.Element<any> => (
   <div className={styles.wrapper}>
     <Helmet title="Texas Camp 2018" />
-    <div className={styles.backgroundWrapper}>
-      <img className={styles.backgroundImage} src={backgroundImage} alt="" />
-    </div>
+    <BackgroundImage />
     <Header />
     <Logo />
     <NewsletterForm />
