d410ac242a1b3062bb3ac4cbee64e1c00b906a4d
kmoon <kristy@amazee.com>
2017-12-12 16:13:19 -0600

    Hook up NotFound page

--- frontend/app/components/App/index.js
+++ frontend/app/components/App/index.js
@@ -10,6 +10,7 @@ import Sessions from 'Sessions';
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import Humans from '!file-loader?name=humans.txt!./humans.txt';
 import NewsOverview from 'NewsOverview';
+import NotFound from 'NotFound';
 import Typekit from 'react-typekit';
 import styles from './styles.css';
 
@@ -25,6 +26,7 @@ const App = (): React.Element<any> =>
       <Route exact path="/sessions" component={Sessions} />
       <Route exact path={Humans} />
       <Route exact path="/news/:page?/" component={NewsOverview} />
+      <Route path="*" component={NotFound} />
     </Switch>
   </div>);
 
