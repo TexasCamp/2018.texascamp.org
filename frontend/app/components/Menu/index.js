3d81e087ffbcb094ee07b1d5ad761a993a506a61
Kathryn McClintock <kccmcck@gmail.com>
2018-01-03 15:58:20 -0600

    TXC-38: Button animation without rotation

--- frontend/app/components/Menu/index.js
+++ frontend/app/components/Menu/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NewsletterForm from 'NewsletterForm';
 import withLogic from 'Menu/logic';
 import styles from './styles.css';
 
@@ -7,42 +6,12 @@ const Menu = ({ isActive, toggleOverlay }) => {
   const activeClass = isActive ? styles.active : styles.inactive;
   return (
     <div className={styles.menu}>
-      <div
+      <button
         className={`${styles.menuButton} ${activeClass}`}
         onClick={toggleOverlay}
-        role="button"
-        tabIndex={0}
       >
         Menu
-      </div>
-      <div className={`${styles.menuOverlay} ${activeClass}`}>
-        <nav>
-          <ul className={styles.menu}>
-            <li className={styles.menuItem}>
-              <a>Home</a>
-            </li>
-            <li className={styles.menuItem}>
-              <a>Speak</a>
-            </li>
-            <li className={styles.menuItem}>
-              <a>Sponsor</a>
-            </li>
-            <li className={styles.menuItem}>
-              <a>Venue</a>
-            </li>
-            <li className={styles.menuItem}>
-              <a>News</a>
-            </li>
-            <li className={styles.menuItemSmall}>
-              <a>Code of Conduct</a>
-            </li>
-            <li className={styles.menuItemSmall}>
-              <a>Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <NewsletterForm />
-      </div>
+      </button>
     </div>
   );
 };
