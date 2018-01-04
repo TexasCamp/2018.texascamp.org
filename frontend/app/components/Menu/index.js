9d20a4810d0add849d3a6137d05db83e36aa6823
Kathryn McClintock <kccmcck@gmail.com>
2018-01-04 10:25:42 -0600

    TXC-38: Latest changes, still in progress

--- frontend/app/components/Menu/index.js
+++ frontend/app/components/Menu/index.js
@@ -12,6 +12,34 @@ const Menu = ({ isActive, toggleOverlay }) => {
       >
         Menu
       </button>
+
+      <div className={`${styles.menuOverlay} ${activeClass}`}>
+        <nav>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}>
+              <a>Home</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a>Speak</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a>Sponsor</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a>Venue</a>
+            </li>
+            <li className={styles.menuItem}>
+              <a>News</a>
+            </li>
+            <li className={styles.menuItemSmall}>
+              <a>Code of Conduct</a>
+            </li>
+            <li className={styles.menuItemSmall}>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
