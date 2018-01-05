f56b3c5e4dad849bbfe3d103370aad0fc5d86506
kmoon <kristy@amazee.com>
2018-01-05 16:34:15 -0600

    TXC-43: Refactoring

--- frontend/app/screens/SplatRouter/screens/BasicPage/index.js
+++ frontend/app/screens/SplatRouter/screens/BasicPage/index.js
@@ -1,5 +1,52 @@
 // @flow
 
-import universal from 'react-universal-component';
+import React from 'react';
+import Helmet from 'react-helmet';
+import Header from 'Header';
+import Menu from 'Menu';
+import Footer from 'Footer';
+import Html from 'Html';
+import styles from './styles.css';
 
-export default universal(import('BasicPage/component'));
+type BasicPageProps = {
+  title: string,
+  body: string,
+};
+
+const BasicPage = ({ title, body }: BasicPageProps): React.Element<any> => {
+  // Format body to:
+  // - Update inline image src to include full url
+  // - Remove all links
+  let formattedBody = body;
+  formattedBody = formattedBody
+    ? formattedBody.replace(
+        'src="/sites/default/files/inline-images/',
+        'src="http://2018.texascamp.org.docker.amazee.io/sites/default/files/inline-images/',
+      )
+    : '';
+
+  return (
+    <div>
+      <Helmet title={title} />
+      <Menu />
+      <div className={styles.contentWrapper}>
+        <Header />
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            {title}
+          </h1>
+          <div className={styles.detail}>
+            <div className={styles.mainContent}>
+              <Html>
+                {formattedBody}
+              </Html>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BasicPage;
