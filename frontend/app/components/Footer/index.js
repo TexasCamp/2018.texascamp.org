93404f856c988f01bb24eeb13cfcb85ae28e0d77
kmoon <kristy@amazee.com>
2018-01-10 13:56:30 -0600

    TXC-41: style changes

--- frontend/app/components/Footer/index.js
+++ frontend/app/components/Footer/index.js
@@ -1,17 +1,26 @@
+// @flow
+
 import React from 'react';
+import Sponsor from 'Sponsor';
+import withSponsorsQuery from 'Sponsors/withSponsorsQuery';
 import styles from './styles.css';
 
-function Footer() {
-  return (
-    <div className={styles.footer}>
+const Footer = ({ loading, sponsors }): React.Element<any> | null =>
+  !loading &&
+  <div className={styles.footer}>
+    <div className={styles.sponsorsWrapper}>
       <div className={styles.sponsors}>
         <p>Platinum Sponsors</p>
-      </div>
-      <div className={styles.credits}>
-        Built by Amazee Labs with Drupal+React and hosted on amazee.io.
+        <div className={styles.sponsorsContainer}>
+          {sponsors
+            .filter(sponsor => sponsor.sponsorLevel === 'Platinum')
+            .map(sponsor => <Sponsor key={sponsor.id} sponsor={sponsor} />)}
+        </div>
       </div>
     </div>
-  );
-}
+    <div className={styles.credits}>
+      Built by Amazee Labs with Drupal+React and hosted on amazee.io.
+    </div>
+  </div>;
 
-export default Footer;
+export default withSponsorsQuery(Footer);
