1610d0dd0a9feab9da6b76c31e8d5c2cc0cce779
Brandon Williams <brandon@amazeelabs.com>
2018-01-11 10:14:13 -0600

    TXC-44 Add unlimited speakers

--- frontend/app/screens/SubmitSession/logic.js
+++ frontend/app/screens/SubmitSession/logic.js
@@ -2,7 +2,28 @@ import withState from 'recompose/withState';
 import withHandlers from 'recompose/withHandlers';
 import compose from 'recompose/compose';
 
-const withFormValues = withState('formValues', 'setFormValues', {});
+const withFormValues = withState(
+  'formValues',
+  'setFormValues',
+  ({ speakerCount }) => {
+    const initial = {
+      title: '',
+      track: '',
+      skillLevel: '',
+      desc: '',
+      bio: '',
+      name: '',
+      email: '',
+      company: '',
+    };
+
+    for (let i = 1; i <= speakerCount; i++) {
+      initial[`speakerName${i}`] = '';
+    }
+
+    return initial;
+  },
+);
 
 const withSetFormValues = withHandlers({
   setFormValue: props => (name, value) =>
@@ -12,4 +33,20 @@ const withSetFormValues = withHandlers({
     }),
 });
 
-export default compose(withFormValues, withSetFormValues);
+const withSpeakerCount = withState('speakerCount', 'setSpeakerCount', 2);
+
+const withSpeakerHandlers = withHandlers({
+  addSpeaker: ({ speakerCount, setSpeakerCount, setFormValue }) => () => {
+    setSpeakerCount(speakerCount + 1);
+    setFormValue(`speakerName${speakerCount + 1}`, '');
+  },
+  removeSpeaker: ({ setSpeakerCount }) => () =>
+    setSpeakerCount(n => (n - 1 < 1 ? 1 : n - 1)),
+});
+
+export default compose(
+  withSpeakerCount,
+  withFormValues,
+  withSetFormValues,
+  withSpeakerHandlers,
+);
