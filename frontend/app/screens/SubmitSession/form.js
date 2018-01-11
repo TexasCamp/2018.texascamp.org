2ba6cfaa914b79b70b32ad947275fa470e86fdc6
Brandon Williams <brandon@amazeelabs.com>
2018-01-11 10:14:13 -0600

    TXC-44 Convert form elements to controlled form elements

--- frontend/app/screens/SubmitSession/form.js
+++ frontend/app/screens/SubmitSession/form.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
-const SessionForm = ({ tracks, skillLevels }) => {
+const SessionForm = ({ tracks, skillLevels, formValues, setFormValue }) => {
+  const saveValue = event => {
+    setFormValue(event.target.id, event.target.value);
+  };
+
   return (
     <form>
       <div>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" required />
+        <input
+          type="text"
+          id="title"
+          value={formValues.title}
+          onChange={saveValue}
+          required
+        />
       </div>
       <div>
         <label htmlFor="track">Track</label>
-        <select id="track" required>
+        <select id="track" value={formValues.track} onBlur={saveValue} required>
+          <option value="">-- Select track --</option>
           {tracks.map(track =>
             (<option key={track.id}>
               {track.name}
@@ -19,7 +30,13 @@ const SessionForm = ({ tracks, skillLevels }) => {
       </div>
       <div>
         <label htmlFor="skillLevel">Skill level</label>
-        <select id="skillLevel" required>
+        <select
+          id="skillLevel"
+          value={formValues.skillLevel}
+          onBlur={saveValue}
+          required
+        >
+          <option value="">-- Select skill level --</option>
           {skillLevels.map(skillLevel =>
             (<option key={skillLevel.id}>
               {skillLevel.name}
@@ -31,30 +48,62 @@ const SessionForm = ({ tracks, skillLevels }) => {
         <legend>Speakers</legend>
         <div>
           <label htmlFor="speakerName">Name</label>
-          <input type="text" id="speakerName" />
+          <input
+            type="text"
+            id="speakerName"
+            value={formValues.speakerName}
+            onChange={saveValue}
+          />
         </div>
       </fieldset>
       <div>
         <label htmlFor="desc">Description</label>
-        <textarea id="desc" required />
+        <textarea
+          id="desc"
+          value={formValues.desc}
+          onChange={saveValue}
+          required
+        />
       </div>
       <div>
         <label htmlFor="bio">Speakers bio</label>
-        <textarea id="bio" required />
+        <textarea
+          id="bio"
+          value={formValues.bio}
+          onChange={saveValue}
+          required
+        />
       </div>
       <fieldset>
         <legend>Contact information</legend>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" required />
+          <input
+            type="text"
+            id="name"
+            value={formValues.name}
+            onChange={saveValue}
+            required
+          />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <input
+            type="email"
+            id="email"
+            value={formValues.email}
+            onChange={saveValue}
+            required
+          />
         </div>
         <div>
           <label htmlFor="company">Company</label>
-          <input type="text" id="company" />
+          <input
+            type="text"
+            id="company"
+            value={formValues.company}
+            onChange={saveValue}
+          />
         </div>
       </fieldset>
       <input type="submit" value="Submit" />
--- frontend/app/screens/SubmitSession/index.js
+++ frontend/app/screens/SubmitSession/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import SessionForm from 'SubmitSession/form';
 import withTermData from 'SubmitSession/withTermData';
+import withLogic from 'SubmitSession/logic';
+import compose from 'recompose/compose';
 
-const SubmitSession = ({ tracks, skillLevels }) => {
-  return <SessionForm tracks={tracks} skillLevels={skillLevels} />;
+const SubmitSession = ({ tracks, skillLevels, formValues, setFormValue }) => {
+  return (
+    <SessionForm
+      tracks={tracks}
+      skillLevels={skillLevels}
+      formValues={formValues}
+      setFormValue={setFormValue}
+    />
+  );
 };
 
-export default withTermData(SubmitSession);
+export default compose(withTermData, withLogic)(SubmitSession);
