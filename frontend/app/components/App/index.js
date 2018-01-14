c2b146765810771913de5fbd2f6cbae3abe1d008
Brandon Williams <brandon@amazeelabs.com>
2018-01-14 15:44:36 -0600

    TXC-46 Add splashpage back at / and move Home to /home

--- frontend/app/components/App/index.js
+++ frontend/app/components/App/index.js
@@ -5,6 +5,7 @@ import 'App/normalize.css';
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Switch, Route } from 'react-router';
+import SplashPage from 'SplashPage';
 import Home from 'Home';
 import Sessions from 'Sessions';
 import Session from 'Session';
@@ -17,20 +18,23 @@ import styles from './styles.css';
 import '../../shared/css/global.css';
 
 const App = (): React.Element<any> =>
-  (<div className={styles.wrapper}>
+  (<div>
     <Helmet
       titleTemplate="Texas Camp 2018 - %s"
       defaultTitle="Texas Camp 2018"
     />
     <Typekit kitId="yde7xbz" />
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/sessions" component={Sessions} />
-      <Route exact path="/sessions/:sessionName/" component={Session} />
-      <Route exact path={Humans} />
-      <Route exact path="/news" component={NewsOverview} />
-      <Route exact path="/news/:newsTitle/" component={News} />
-      <Route path="*" component={SplatRouter} />
+      <Route exact path="/" component={SplashPage} />
+      <div className={styles.wrapper}>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/sessions" component={Sessions} />
+        <Route exact path="/sessions/:sessionName/" component={Session} />
+        <Route exact path={Humans} />
+        <Route exact path="/news" component={NewsOverview} />
+        <Route exact path="/news/:newsTitle/" component={News} />
+        <Route path="*" component={SplatRouter} />
+      </div>
     </Switch>
   </div>);
 
---
+++ frontend/app/screens/SplashPage/components/BackgroundImage/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import horizon from 'images/horizon.png';
+import van from 'images/van.png';
+import cow from 'images/cow.png';
+import styles from './styles.css';
+
+class BackgroundImage extends React.Component {
+  componentDidMount() {
+    if (global.window) {
+      global.window.addEventListener('mousemove', this.updatePosition, false);
+    }
+  }
+  updatePosition = e => {
+    if (!this.resizeTimeout) {
+      this.resizeTimeout = setTimeout(() => {
+        this.resizeTimeout = null;
+        const getYFromCenter = e.view.innerHeight / 2 - e.clientY;
+        const getXFromCenter = e.view.innerWidth / 2 - e.clientX;
+        if (global.document) {
+          if (global.document.getElementById('horizon')) {
+            global.document.getElementById('horizon').style.top = `${0.015 *
+              getYFromCenter}px`;
+            global.document.getElementById(
+              'horizon',
+            ).style.marginLeft = `${0.015 * getXFromCenter}px`;
+          }
+          if (global.document.getElementById('cow')) {
+            global.document.getElementById('cow').style.top = `${0.005 *
+              getYFromCenter}px`;
+            global.document.getElementById('cow').style.marginLeft = `${0.005 *
+              getXFromCenter}px`;
+          }
+        }
+      }, 75);
+    }
+  };
+  render() {
+    return (
+      <div className={styles.backgroundWrapper}>
+        <div className={styles.horizon}>
+          <img
+            className={styles.backgroundImage}
+            src={horizon}
+            alt=""
+            id="horizon"
+          />
+        </div>
+        <div className={styles.van}>
+          <img className={styles.backgroundImage} src={van} alt="" />
+        </div>
+        <div className={styles.cow}>
+          <img className={styles.backgroundImage} src={cow} alt="" id="cow" />
+        </div>
+      </div>
+    );
+  }
+}
+export default BackgroundImage;
