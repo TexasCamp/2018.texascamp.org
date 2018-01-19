67364b32da7b9df67babcec25090a8f92a390dd3
kmoon <kristy@amazee.com>
2018-01-18 23:20:08 -0600

    TXC-42: Add metatags

--- frontend/app/screens/Home/index.js
+++ frontend/app/screens/Home/index.js
@@ -11,26 +11,13 @@ import NewsTeaser from 'NewsTeaser';
 import withNewsQuery from 'NewsOverview/withNewsQuery';
 import Sponsor from 'Sponsor';
 import withSponsorsQuery from 'Sponsors/withSponsorsQuery';
-import favicon from '!file-loader?name=favicon.ico!favicons/favicon.ico';
 import styles from 'Home/styles.css';
-import appleTouchIcon from 'favicons/apple-touch-icon.png';
-import favicon32x32 from 'favicons/favicon-32x32.png';
-import favicon16x16 from 'favicons/favicon-16x16.png';
-import safariPinnedTab from 'favicons/safari-pinned-tab.svg';
 
 const Home = ({ loading, newsList, sponsors }): React.Element<any> | null =>
   !loading &&
   <div className={styles.wrapper}>
     <Helmet>
       <title>Texas Camp 2018</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#ef5a47" />
-      <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-      <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
-      <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
-      <link rel="mask-icon" href={safariPinnedTab} color="#ef5a47" />
-      <link rel="shortcut icon" href={favicon} />
     </Helmet>
     <Menu />
     <div className={styles.contentWrapper}>
