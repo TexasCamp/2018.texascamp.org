518d50298614391d4515c65f52838dd3eb2172ab
kmoon <kristy@amazee.com>
2018-01-29 16:21:43 -0600

    TXC-73: Change external sponsor link to a tag

--- frontend/app/components/Sponsor/index.js
+++ frontend/app/components/Sponsor/index.js
@@ -1,7 +1,6 @@
 // @flow
 
 import React from 'react';
-import Link from 'AsyncLink';
 import type { SponsorT } from 'types/SponsorT';
 import styles from './styles.css';
 
@@ -13,13 +12,13 @@ const Sponsor = ({ sponsor }: SponsorProps) => {
   return (
     <div className={styles.sponsorWrapper}>
       {sponsor.sponsorUrl
-        ? <Link to={sponsor.sponsorUrl}>
+        ? <a href={sponsor.sponsorUrl}>
           {sponsor.sponsorLevel !== 'Individual' && sponsor.image.url
               ? <img src={sponsor.image.url} alt={sponsor.image.alt} />
               : <p>
                 {sponsor.title}
               </p>}
-        </Link>
+        </a>
         : <div>
           {sponsor.sponsorLevel !== 'Individual'
               ? <img src={sponsor.image.url} alt={sponsor.image.alt} />
